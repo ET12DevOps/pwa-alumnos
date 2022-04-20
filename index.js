d70c50408400d38b8d1a497b5068ac7da272978d
jvelazquez <jonathanvgms@gmail.com>
2022-04-20 08:40:50 -0300

    Add navigation between html files

--- index.js
+++ index.js
@@ -14,12 +14,31 @@ const button_agregar = document.getElementById('agregar_alumno')
 
 const nombre_alumno = document.getElementById('nombre')
 const apellido_alumno = document.getElementById('apellido')
+const dni_alumno = document.getElementById('dni')
+const usuario_alumno = document.getElementById('usuario')
+const contrasenia_alumno = document.getElementById('contrasenia')
 
 // forma moderna
 // funcion definida con 'funcion de flecha'
 button_agregar.addEventListener('click', () => {
     //para acceder al contenido del objeto (input) se debe colocar el atributo 'value'
-    console.log(`Nombre: ${nombre_alumno.value}`)
-    console.log(`Apellido: ${apellido_alumno.value}`)
+
+    // crea un objeto js Alumno
+    const alumno = {
+        nombre: nombre_alumno.value,
+        apellido: apellido_alumno.value,
+        dni: parseInt(dni_alumno.value),
+        usuario: usuario_alumno.value,
+        contrasenia: contrasenia_alumno.value
+    }
+
+    // acceder a atributos del objeto mediante el punto
+    console.log(alumno.nombre)
+
+    // informa objeto Alumno en formato JSON
+    console.log(alumno)
+
+    // informa objeto Alumno en formato de tabla
+    console.table(alumno)
 })
 
